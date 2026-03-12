9d5bb5dba2afcff5c70ff2e3d2388201db2bf506
肖林捷 <152128494+LinJieXiao-XLJ@users.noreply.github.com>
2026-03-12 17:33:45 +0800

    Change the report file name

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ -x "./build/test/$filename" ]; then
    echo "/******* Start test ********/"
    cd ./build/test
    # 执行文件
-   ./main --gtest_output="json:cpp_session_test_report.json"
+   ./main --gtest_output="json:cpp_tsfile_test_report.json"
 else
    # 文件不存在或不可执行，打印错误信息
    echo "!!!错误:build/test 目录下 $filename 文件不存在或不是可执行文件。"
